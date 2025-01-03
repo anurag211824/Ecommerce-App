38a5aec7521c0807dc23faf9a63e4693a5a69516
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2025-01-03 13:34:42 +0530

    intialized cartController.js

---
+++ Backend/controllers/cartController.js
@@ -0,0 +1,19 @@
+//Add products to Usercart
+const addToCart=async(req,res)=>{
+
+}
+
+
+//Update Usercart
+const updateCart=async(req,res)=>{
+
+}
+
+
+//Get Usercart data
+const getUserCart=async(req,res)=>{
+
+}
+
+
+export{addToCart,updateCart,getUserCart}
